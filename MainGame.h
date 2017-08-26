02bbd35bf205d26361e6f443c0c45637e0e39d4e
Luis <luis@luis.com>
2017-08-25 23:00:40 -0500

    primera clase

---
+++ MainGame.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SDL\SDL.h>
+
+enum class GameState {
+	PLAY, EXIT
+};
+
+class MainGame{
+private:
+	int _width;
+	int _height;
+	void init();
+	SDL_Window* _window;
+	void processInput();
+public:
+	MainGame();
+	~MainGame();
+	GameState _gameState;
+	void run();
+	void draw();
+	void update();
+};
+
